2555dae3ad6f7b54fd50ea25be4112bee54ac2aa
Addam Wassel <awassel@d-awassel2-mbp.local>
2015-03-29 14:30:21 -0400

    Initial commit of Basic framework

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+var syncsrc = [ '**', '!**/sass/**' ];
+
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+
+    sync: {
+      dev: {
+        files: [
+          { cwd: 'src', src: syncsrc, dest: 'build/dev' }
+        ]
+      },
+      release: {
+        files: [
+          { cwd: 'src', src: syncsrc, dest: 'build/release' }
+        ],
+        verbose: true
+      }
+    },
+
+
+    compass: {
+      options: { sassDir: 'src/sass', require: [ 'breakpoint' ]},
+      dev: { options: { cssDir: 'build/dev/stylesheets' } },
+      release: { options: { outputStyle: 'expanded', cssDir: 'build/release/stylesheets' } }
+    },
+
+    open: { file: { path: 'build/release/bcc-output.txt' } },
+
+    watch: {
+      css: { files: 'src/**/*.scss', tasks: ['compass:dev'] }
+    }
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-open');
+
+
+  // Default task
+  grunt.registerTask('default', [
+    'sync',
+    'compass',
+    'watch'
+  ]);
+
+
+  // Target-specific tasks
+
+  // Dev
+  grunt.registerTask('dev', [
+    'sync:dev',
+    'compass:dev'
+  ]);
+
+  // Release
+  grunt.registerTask('release', [
+    'sync:release',
+    'compass:release',
+    'open'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic CSS Grid Framework
+
+Basic is a CSS framework built on top of [Compass](http://compass-style.org/).
